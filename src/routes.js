3f3ce303f60862bbb8b4f3b509729538fa9a9bcb
Julian Barbosa <bjvlixn@gmail.com>
2022-01-12 20:15:54 -0300

    add contacts POST route

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
+router.post('/contacts', ContactController.store);
 
 module.exports = router;
