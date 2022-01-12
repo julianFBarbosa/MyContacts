f6e73fc2d568aab9d90765f788d4ae191514a4f5
Julian Barbosa <bjvlixn@gmail.com>
2022-01-12 19:28:21 -0300

    add repositories

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-promise-executor-return */
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    id: v4(),
+    name: 'Julian',
+    email: 'barbosabjf@gmail.com',
+    phone: '123123123',
+    category_id: v4(),
+  }, {
+    id: v4(),
+    name: 'Jose',
+    email: 'lalalal@gmail.com',
+    phone: '1111111111',
+    category_id: v4(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
