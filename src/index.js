276121b74fb7949c5ae7993e707555ff9f6a2085
Julian Barbosa <bjvlixn@gmail.com>
2022-01-12 19:28:40 -0300

    add index.js entrypoint

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000');
+});
