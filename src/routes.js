91698c7532972c09f3275289335daf82329519a6
Julian Barbosa <bjvlixn@gmail.com>
2022-01-13 20:00:21 -0300

    add route to update contact

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
 
 module.exports = router;
