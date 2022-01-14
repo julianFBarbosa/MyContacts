f594505d168e0ab0116a7b8e75641bd8561d6df7
Julian Barbosa <bjvlixn@gmail.com>
2022-01-14 00:05:32 -0300

    add database/schema

---
+++ src/database/schema.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE mycontacts;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS categories (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contacts (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR UNIQUE,
+  phone VARCHAR,
+  category_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id)
+);
