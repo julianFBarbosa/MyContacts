ccec814df9331461c01991eaa6d11600ed15b08f
Julian Barbosa <bjvlixn@gmail.com>
2022-01-15 00:58:36 -0300

    add GET/POST routes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const CategoryController = require('./app/controllers/CategoryController');
 const ContactController = require('./app/controllers/ContactController');
 
 const router = Router();
@@ -8,5 +9,8 @@ router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
