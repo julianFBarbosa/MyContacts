0a8a15a6464e1ca9ebcafab771e962b9922c6bf6
Julian Barbosa <bjvlixn@gmail.com>
2022-01-15 00:59:55 -0300

    refactor DELETE contact method

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -70,12 +70,6 @@ class ContactController {
 
   async delete(request, response) {
     const { id } = request.params;
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      // 404: Not Found
-      return response.status(404).json({ error: 'User not found' });
-    }
 
     await ContactsRepository.delete(id);
     // 204: No Content
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,22 +1,4 @@
-/* eslint-disable no-promise-executor-return */
-const { v4 } = require('uuid');
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Julian',
-    email: 'barbosabjf@gmail.com',
-    phone: '123123123',
-    category_id: v4(),
-  }, {
-    id: v4(),
-    name: 'Jose',
-    email: 'lalalal@gmail.com',
-    phone: '1111111111',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(order = 'ASC') {
@@ -68,11 +50,12 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query(`
+      DELETE FROM contacts
+      WHERE id = $1
+    `, [id]);
+    return deleteOp;
   }
 }
 
