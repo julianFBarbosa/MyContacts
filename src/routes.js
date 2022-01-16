5a4995c8b77237634e20896948fdd28239355bb1
Julian Barbosa <bjvlixn@gmail.com>
2022-01-16 17:15:33 -0300

    add show category route

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
 
 router.get('/categories', CategoryController.index);
+router.get('/categories/:id', CategoryController.show);
 router.post('/categories', CategoryController.store);
 
 module.exports = router;
