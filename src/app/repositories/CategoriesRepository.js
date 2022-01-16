c6835208d4d0de973fa47915b2d6b45e7f1aad5a
Julian Barbosa <bjvlixn@gmail.com>
2022-01-16 17:19:45 -0300

    add findByName method

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -10,6 +10,14 @@ class CategoryRepository {
     return rows;
   }
 
+  async findByName(name) {
+    const [row] = await db.query(`
+       SELECT * FROM categories WHERE name = $1
+    `, [name]);
+
+    return row;
+  }
+
   async create({ name }) {
     const [row] = await db.query(`
       INSERT INTO categories(name)
