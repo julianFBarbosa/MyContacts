c8d614da4c4f81099a762e73edffbc86ff37a75c
Julian Barbosa <bjvlixn@gmail.com>
2022-01-16 23:33:40 -0300

    add delete and put routes to /categories endpoint

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,8 @@ router.put('/contacts/:id', ContactController.update);
 
 router.get('/categories', CategoryController.index);
 router.get('/categories/:id', CategoryController.show);
+router.delete('/categories/:id', CategoryController.delete);
 router.post('/categories', CategoryController.store);
+router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
