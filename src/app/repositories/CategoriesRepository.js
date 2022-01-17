d98878932bc62eda4552d2a6dbbc375888c48fb5
Julian Barbosa <bjvlixn@gmail.com>
2022-01-16 23:34:40 -0300

    add update and delete queries

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -4,7 +4,8 @@ class CategoryRepository {
   async findAll(order = 'ASC') {
     const direction = order.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
     const rows = await db.query(`
-      SELECT * FROM categories ORDER BY name ${direction} 
+      SELECT * FROM categories 
+      ORDER BY name ${direction}
     `);
 
     return rows;
@@ -26,6 +27,28 @@ class CategoryRepository {
     `, [name]);
 
     return row;
+  }
+
+  async update(id, {
+    category,
+  }) {
+    const [row] = await db.query(`
+      UPDATE categories
+      SET name = $1
+      WHERE id = $2
+      RETURNING *
+    `, [category, id]);
+
+    return row;
+  }
+
+  async delete(id) {
+    const deleteOp = await db.query(`
+      DELETE FROM categories
+      WHERE id = $1
+    `, [id]);
+
+    return deleteOp;
   }
 }
 
