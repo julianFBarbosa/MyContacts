265847a571d1182f7a998afe27834a71629438d6
Julian Barbosa <bjvlixn@gmail.com>
2022-01-24 20:06:48 -0300

    add middleware to error handlings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+require('express-async-errors');
+
 const router = require('./routes');
 
 const app = express();
@@ -6,6 +8,13 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+app.use((error, request, response, next) => {
+  console.log('#### Error Handler ####');
+  console.log(error);
+  response.sendStatus(500);
+});
+
 app.listen(3000, () => {
+  // eslint-disable-next-line no-console
   console.log('Server started at http://localhost:3000');
 });
