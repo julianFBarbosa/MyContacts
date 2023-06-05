bde94e1740ea4f76f0ee02e18898c7f571c76377
Julian Barbosa <barbosabjf@gmail.com>
2023-06-04 23:43:37 -0300

    Update App.js
    Atualizado parágrafo do App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Oladfdfdsdsdsdsdsdsdiohuyyufiftyuujrdr
+          Olá
         </p>
         <a
           className="App-link"
